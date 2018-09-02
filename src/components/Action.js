bd3af4c06928150f30ed72b411c8ad628ab539c8
“Drew” <drewloope@gmail.com>
2018-09-02 11:58:04 -0400

    // Modal

--- src/components/Action.js
+++ src/components/Action.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const Action = (props) => {
-    return (
+const Action = (props) => (
         <div>
             <button onClick={props.handlePick} disabled={!props.hasOptions}>What Should I Do?</button>
         </div>
     );
-};
 
 export default Action;
